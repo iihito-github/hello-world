c90af9cbf1fd7a96f799513ec7ad07c5de848df3
iihito-github <iihito.mobile@gmail.com>
2018-03-04 16:16:48 +0900

    一応、ハローワールドしました。

--- main.js
+++ main.js
@@ -26,5 +26,27 @@ function render() {
   // 全体をクリア
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillRect(10, 10, 100, 100);
+  //矩形
+  ctx.beginPath();
+  ctx.fillStyle = 'rgb(192, 80, 77)'; // 赤
+  ctx.rect(10, 10, 100, 100);
+  ctx.fill();
+
+  //円を書く
+  ctx.beginPath();
+  ctx.fillStyle = 'rgb(155, 187, 89)'; // 緑
+  ctx.arc(45, 95, 35, 0, Math.PI*2, false);
+  ctx.fill();
+
+  //円を描く
+  ctx.beginPath();
+  ctx.fillStyle = 'rgb(128, 100, 162)'; // 紫
+  ctx.arc(95, 95, 35, 0, Math.PI*2, false);
+  ctx.fill();
+
+  //文字を書く
+  ctx.beginPath();
+  ctx.fillStyle = 'rgb(0, 0, 0)'; // 黒
+  ctx.strokeText("Hello world", 50,50);
+  ctx.fill();
 }
